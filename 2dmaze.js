f57ea22a9e6b3b880c8b8fc8efa9de06e2017d8c
Kevin Perez <kevper2019@gmail.com>
2023-09-16 22:22:17 -0400

    Reupload latest version of files

--- 2dmaze.js
+++ 2dmaze.js
@@ -6,8 +6,17 @@ var numColumns = 3;
 var numRows = 3;
 
 var entranceStartpoint;
+var originPoint; 
+
+var rot;
+var wallLength;
+var ratCell;
 
 var vertices = [];
+
+var maze = [];
+var rat = [];
+
 
 window.onload = function init()
 {
@@ -47,15 +56,108 @@ window.onload = function init()
 
     document.getElementById("rowSlider").onchange = function(event) {
         numRows = event.target.value;
+        document.getElementById("rowText").innerHTML = numRows;
     };
 
     document.getElementById("columnSlider").onchange = function(event) {
         numColumns = event.target.value;
+        document.getElementById("columnText").innerHTML = numColumns;
         
     };
 
     document.getElementById("generateBtn").onclick = function() {
         drawMaze();
+        gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+        render();
+    };
+
+    window.onkeydown = function(event) {
+        let wall;
+
+        var key = event.keyCode;
+        switch( key ) {
+
+          //Left
+          case 37:
+            //console.log("left");
+
+            rot = rot + Math.PI/2;
+            vertices = maze;
+            vertices = vertices.concat(drawRat(ratCell, rot));
+            
+
+            break;
+
+          //Up
+          case 38:
+            //console.log("up");
+            /*
+            wall = vec2(ratCell[0], ratCell[1] + 1);
+            if (!wallExists(vertices, wall, true)) {
+                ratCell = wall;
+            }
+            
+            vertices = maze;
+            vertices = vertices.concat(drawRat(ratCell, Math.PI/2));
+            */
+            //console.log("hi")
+            //console.log(Math.round(Math.cos(rot)) + " " + Math.round(Math.sin(rot)));
+            //right
+            if (Math.round(Math.cos(rot)) == 1 && Math.round(Math.sin(rot)) == 0) {
+                
+                wall = vec2(ratCell[0] + 1, ratCell[1]);
+                if (!wallExists(vertices, wall, false)) {
+                    ratCell = wall;
+                }    
+                
+            //up
+            } else if (Math.round(Math.cos(rot)) == 0 && Math.round(Math.sin(rot)) == 1) {
+                //console.log("hi2")
+                wall = vec2(ratCell[0], ratCell[1] + 1);
+                if (!wallExists(vertices, wall, true)) {
+                    ratCell = wall;
+                }
+            //right
+            } else if (Math.round(Math.cos(rot)) == -1 && Math.round(Math.sin(rot)) == 0) {
+                if (!wallExists(vertices, ratCell, false)) {
+                    ratCell = vec2(ratCell[0] - 1, ratCell[1]);
+                }
+            } else if (Math.round(Math.cos(rot)) == 0 && Math.round(Math.sin(rot)) == -1) {
+                if (!wallExists(vertices, ratCell, true)) {
+                    ratCell = vec2(ratCell[0], ratCell[1] - 1);
+                }
+            }
+
+            vertices = maze;
+            vertices = vertices.concat(drawRat(ratCell, rot))
+            break;
+
+          //Right
+          case 39:
+            //console.log("right");
+
+            rot = rot - Math.PI/2;
+
+            vertices = maze;
+            vertices = vertices.concat(drawRat(ratCell, rot));
+
+            break;
+          
+          //Down
+          case 40:
+            //console.log("down");
+            /*
+            if (!wallExists(vertices, ratCell, true)) {
+                ratCell = vec2(ratCell[0], ratCell[1] - 1);
+            }
+            
+            vertices = maze;
+            vertices = vertices.concat(drawRat(ratCell, Math.PI * 3/2));
+            */
+            break;
+
+        }
+
         gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
         render();
     };
@@ -71,16 +173,14 @@ function render()
 
 function drawMaze() {
 
-    vertices = [];
+    let mazePoints = [];
 
     //STEP 1: Create the grid
-    var wallLength = 2 / (Math.max(numColumns, numRows) + 2);
-    var originPoint = vec2(-(wallLength*numColumns)/2, -(wallLength*numRows)/2);
+    wallLength = 2 / (Math.max(numColumns, numRows) + 2);
+    originPoint = vec2(-(wallLength*numColumns)/2, -(wallLength*numRows)/2);
 
-    var numCells = numRows * numColumns;
-
-    var startpointPool = [];
-    var endpointPool = [];
+    let startpointPool = [];
+    let endpointPool = [];
 
     // loop to populate grid with line segments
     for (let i = 0; i <= numColumns; i++) {
@@ -89,11 +189,11 @@ function drawMaze() {
             //add each vertical line segment
             if (j < numRows) {
 
-                var vertStartpoint = vec2(originPoint[0] + i * wallLength, originPoint[1] + j*wallLength);
-                var vertEndpoint = vec2(originPoint[0] + i * wallLength, originPoint[1] + j*wallLength + wallLength);
+                let vertStartpoint = vec2(originPoint[0] + i * wallLength, originPoint[1] + j*wallLength);
+                let vertEndpoint = vec2(originPoint[0] + i * wallLength, originPoint[1] + j*wallLength + wallLength);
 
-                vertices.push(vertStartpoint);
-                vertices.push(vertEndpoint); 
+                mazePoints.push(vertStartpoint);
+                mazePoints.push(vertEndpoint); 
                 
                 //Puts only the inner walls into a pool
                 if (i != 0 && i != numColumns) {
@@ -105,11 +205,11 @@ function drawMaze() {
             //add each horizontal line segment
             if (i < numColumns) {
 
-                var horiStartpoint = vec2(originPoint[0] + i * wallLength, originPoint[1] + j*wallLength);
-                var horiEndpoint = vec2(originPoint[0] + i * wallLength + wallLength, originPoint[1] + j*wallLength);
+                let horiStartpoint = vec2(originPoint[0] + i * wallLength, originPoint[1] + j*wallLength);
+                let horiEndpoint = vec2(originPoint[0] + i * wallLength + wallLength, originPoint[1] + j*wallLength);
 
-                vertices.push(horiStartpoint);
-                vertices.push(horiEndpoint);
+                mazePoints.push(horiStartpoint);
+                mazePoints.push(horiEndpoint);
                 
                 //Puts only the inner walls into a pool
                 if (j != 0 && j != numRows) {
@@ -124,11 +224,11 @@ function drawMaze() {
     for (let i = originPoint[0]; i <= -originPoint[0]; i+= wallLength) {
 
         for (let j = originPoint[1]; j <= -originPoint[1]; j+= wallLength) {
-            vertices.push(vec2(i,j));
-            vertices.push(vec2(i, j + wallLength));
+            mazePoints.push(vec2(i,j));
+            mazePoints.push(vec2(i, j + wallLength));
 
-            vertices.push(vec2(i,j));
-            vertices.push(vec2(i + wallLength, j));
+            mazePoints.push(vec2(i,j));
+            mazePoints.push(vec2(i + wallLength, j));
         }
     }
     */
@@ -137,7 +237,7 @@ function drawMaze() {
     //STEP 2: Run Kruskal's algorithm to generate the inner maze itself
     
     //First, create array of arrays that'll
-    var cellSetArray = [];
+    let cellSetArray = [];
 
     for (let i = 0; i < numColumns; i ++) {
         for (let j = 0; j < numRows; j++) {
@@ -148,17 +248,17 @@ function drawMaze() {
     //var iteration = 1; 
     while (cellSetArray.length > 1) {
         //Gets a random inner wall
-        var randIndex = getRandomInt(0, startpointPool.length);
-        var removeStartpoint = startpointPool[randIndex];
-        var removeEndpoint = endpointPool[randIndex];
+        let randIndex = getRandomInt(0, startpointPool.length);
+        let removeStartpoint = startpointPool[randIndex];
+        let removeEndpoint = endpointPool[randIndex];
 
-        var cell1;
-        var cell2;
+        let cell1;
+        let cell2;
 
         //These vars denote where the inner walls start. We can also use them to determine
         //which cells we combine.
-        var centerX = Math.round((removeStartpoint[0] - originPoint[0]) / wallLength);
-        var centerY = Math.round((removeStartpoint[1] - originPoint[1]) / wallLength);
+        let centerX = Math.round((removeStartpoint[0] - originPoint[0]) / wallLength);
+        let centerY = Math.round((removeStartpoint[1] - originPoint[1]) / wallLength);
 
         //Checks whether wall is vertical or horizontal
         if (removeStartpoint[1] < removeEndpoint[1]) {
@@ -185,8 +285,8 @@ function drawMaze() {
         if (cell1SetIndex != cell2SetIndex) {
 
             //console.log("REMOVE THE WALL");
-            //Deletes inner wall from vertices
-            removeWall(vertices, removeStartpoint, removeEndpoint);
+            //Deletes inner wall from mazePoints
+            removeWall(mazePoints, removeStartpoint, removeEndpoint);
 
             //Combines cell2's set onto cell1's set, then removes the previous set from cellSetArray
             cellSetArray[cell1SetIndex] = cellSetArray[cell1SetIndex].concat(cellSetArray[cell2SetIndex]);
@@ -203,10 +303,10 @@ function drawMaze() {
     
     //STEP 3: remove two walls on the outer left and right walls for the entrance and exit
 
-    /*
-    //I can make it so that the entrance and exit is on the further ends (ie if more columns than rows,
+    
+    //I can also make it so that the entrance and exit is on the further ends (ie if more columns than rows,
     //then the the exits will be on the far left and right walls)
-    if (numColumns >= numRows) {
+    if (+numColumns >= +numRows) {
         var entranceLoc = getRandomInt(0, numRows);
         entranceStartpoint = vec2(originPoint[0], originPoint[1] + entranceLoc * wallLength);
         var entranceEndpoint = vec2(entranceStartpoint[0], entranceStartpoint[1] + wallLength);
@@ -214,6 +314,7 @@ function drawMaze() {
         var exitLoc = getRandomInt(0, numRows);
         var exitStartpoint = vec2(originPoint[0] + numColumns * wallLength, originPoint[1] + exitLoc * wallLength);
         var exitEndpoint = vec2(exitStartpoint[0], exitStartpoint[1] + wallLength);
+        //console.log("long or equal");
     } else {
         var entranceLoc = getRandomInt(0, numColumns);
         entranceStartpoint = vec2(originPoint[0] + entranceLoc * wallLength, originPoint[1] + numRows * wallLength);
@@ -223,20 +324,37 @@ function drawMaze() {
         var exitStartpoint = vec2(originPoint[0] + exitLoc * wallLength, originPoint[1]);
         var exitEndpoint = vec2(exitStartpoint[0] + wallLength, exitStartpoint[1]);
 
+        //console.log("tall");
     }
-    */
-    
-    var entranceLoc = getRandomInt(0, numRows);
-    entranceStartpoint = vec2(originPoint[0], originPoint[1] + entranceLoc * wallLength);
-    var entranceEndpoint = vec2(entranceStartpoint[0], entranceStartpoint[1] + wallLength);
 
-    var exitLoc = getRandomInt(0, numRows);
-    var exitStartpoint = vec2(originPoint[0] + numColumns * wallLength, originPoint[1] + exitLoc * wallLength);
-    var exitEndpoint = vec2(exitStartpoint[0], exitStartpoint[1] + wallLength);
-    
+    removeWall(mazePoints, entranceStartpoint, entranceEndpoint);
+    removeWall(mazePoints, exitStartpoint, exitEndpoint);
 
-    removeWall(vertices, entranceStartpoint, entranceEndpoint);
-    removeWall(vertices, exitStartpoint, exitEndpoint);
+    //STEP 4: Create a "mouse" that starts at the entrance of the maze
+
+    let ratLoc;
+    //checks the orientation of the start point
+    if (entranceStartpoint[0] < entranceEndpoint[0]) {
+        rot = Math.PI*(3/2);
+        ratLoc = entranceStartpoint;
+    } else {
+        rot = 0;
+        ratLoc = vec2(entranceStartpoint[0] - wallLength, entranceStartpoint[1]);
+    }
+
+
+    let ratCellX = Math.round((ratLoc[0] - originPoint[0]) / wallLength);
+    let ratCellY = Math.round((ratLoc[1] - originPoint[1]) / wallLength);
+    ratCell = vec2(ratCellX, ratCellY);
+    
+    let ratPoints = drawRat(ratCell, rot);
+
+    vertices = mazePoints;
+    vertices = vertices.concat(ratPoints);
+
+    maze = mazePoints;
+    //rat = ratPoints;
+
     
 }
 
@@ -245,6 +363,34 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
+//Returns array of rat points
+function drawRat(ratCell, rot) {
+    let ratPoints = [];
+
+    let ratLocX = originPoint[0] + ratCell[0] * wallLength;
+    let ratLocY = originPoint[1] + ratCell[1] * wallLength;
+
+    let ratLoc = vec2(ratLocX, ratLocY);
+
+    let ratCenter = vec2(ratLoc[0] + 0.5*wallLength, ratLoc[1] + 0.5*wallLength);
+    
+    let wallFraction = wallLength*0.25;
+
+    let nose = vec2(ratCenter[0] + wallFraction, ratCenter[1]);
+    let leftLeg = vec2(ratCenter[0] - wallFraction, ratCenter[1] + wallFraction);
+    let rightLeg = vec2(ratCenter[0] - wallFraction, ratCenter[1] - wallFraction);
+
+    let noseRot = rotate(nose, ratCenter, rot);
+    let leftLegRot = rotate(leftLeg, ratCenter, rot);
+    let rightLegRot = rotate(rightLeg, ratCenter, rot);
+
+    ratPoints.push(noseRot, leftLegRot);
+    ratPoints.push(leftLegRot, rightLegRot);
+    ratPoints.push(rightLegRot, noseRot);
+
+    return ratPoints;
 }
 
 //Finds the index of the set that a given cell is in
@@ -261,11 +407,42 @@ function findCellSet(cellSetArray, cell) {
 }
 
 //Removes a wall given its startpoint and endpoint
-function removeWall(vertices, startpoint, endpoint) {
-    for (let i = 0; i < vertices.length; i++) {
-        if (equal(vertices[i], startpoint) && equal(vertices[i+1], endpoint)) {
-            vertices.splice(i, 2);
+function removeWall(mazePoints, startpoint, endpoint) {
+    
+    for (let i = 0; i < mazePoints.length; i+=2) {
+        if (equal(mazePoints[i], startpoint) && equal(mazePoints[i+1], endpoint)) {
+            mazePoints.splice(i, 2);
             break;
         }
     }
+    
+}
+
+//Checks if a wall exists
+function wallExists(mazePoints, startCell, isHorizontal) {
+
+    let startpoint = vec2(originPoint[0] + startCell[0] * wallLength, originPoint[1] + startCell[1] * wallLength);
+
+    let endpoint;
+
+    if (isHorizontal) {
+        endpoint = vec2(startpoint[0] + wallLength, startpoint[1]);
+    } else {
+        endpoint = vec2(startpoint[0], startpoint[1] + wallLength);
+    }
+
+    for (let i = 0; i < mazePoints.length; i+=2) {
+        if (equal(mazePoints[i], startpoint) && equal(mazePoints[i+1], endpoint)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//Rotates a point about a center point, by a given value of radians
+function rotate(point, center, rot) {
+    let resultX = (point[0] - center[0]) * Math.cos(rot) - (point[1] - center[1]) * Math.sin(rot) + center[0];
+    let resultY = (point[0] - center[0]) * Math.sin(rot) + (point[1] - center[1]) * Math.cos(rot) + center[1];
+
+    return vec2(resultX, resultY);
 }
